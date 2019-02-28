220b4758746d1af3a6770aa6742e759570012794
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-02-28 22:09:29 +0100

    Adding nav button blinkin when clicked and moved to section

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -2,8 +2,62 @@ const burgerMenu = document.querySelector(".burger-menu");
 
 burgerMenu.addEventListener("click", turnOnMenu);
 
+const navStart = document.querySelector("nav ul li .a-start");
+const navAboutMe = document.querySelector("nav ul li .a-about-me");
+const navWorkflow = document.querySelector("nav ul li .a-workflow");
+const navProjects = document.querySelector("nav ul li .a-projects");
+const navContact = document.querySelector("nav ul li .a-contact");
+
+const navTab = [navStart, navAboutMe, navWorkflow, navProjects, navContact];
+
+// function navToggleBackground() {
+
+//     navTab.forEach(function (Element) {
+//         if (Element.classList.contains("activeSection")) {
+//         Element.classList.remove("activeSection");
+//         }
+//     });
+//     this.classList.add("activeSection");
+// };
+
+// navTab.forEach(function (navTabElement) {
+//     navTabElement.addEventListener("click", navBtnBackground);
+// });
+// navStart.addEventListener("click", navToggleBackground());
+navStart.addEventListener("click", function () {
+    navTab.forEach(function (Element) {
+        Element.classList.remove("activeSection");
+    });
+    navStart.classList.add("activeSection");
+});
+navAboutMe.addEventListener("click", function () {
+    navTab.forEach(function (Element) {
+        Element.classList.remove("activeSection");
+    });
+    navAboutMe.classList.add("activeSection");
+});
+navWorkflow.addEventListener("click", function () {
+    navTab.forEach(function (Element) {
+        Element.classList.remove("activeSection");
+    });
+    navWorkflow.classList.add("activeSection");
+});
+navProjects.addEventListener("click", function () {
+    navTab.forEach(function (Element) {
+        Element.classList.remove("activeSection");
+    });
+    navProjects.classList.add("activeSection");
+});
+navContact.addEventListener("click", function () {
+    navTab.forEach(function (Element) {
+        Element.classList.remove("activeSection");
+    });
+    navContact.classList.add("activeSection");
+});
+
+
+
 function turnOnMenu() {
-    console.log("click");
     document.querySelector(".burger-menu .fas").classList.toggle('fa-bars');
     document.querySelector(".burger-menu .fas").classList.toggle('fa-angle-double-up');
     document.querySelector("nav ul").classList.toggle('clicked');
